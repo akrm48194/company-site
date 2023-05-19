2306a1efd52c87285489be973a50c873676a6cc2
赤松晏奈 <akamatsuanna@akamatsuannanoMacBook-Pro.local>
2023-05-19 13:22:08 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+new WOW().init();
+
+jQuery(".drawer-icon").on("click", function(e) {
+	e.preventDefault();
+
+	jQuery('.drawer-icon').toggleClass('is-active');
+  jQuery('.drawer-content').toggleClass('is-active');
+  jQuery('.drawer-background').toggleClass('is-active');
+	return false;
+});
